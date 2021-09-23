3ea4c9bc61e3c999469c1af4394aff0e2a0221d4
pot-code <136290354@qq.com>
2021-09-23 12:10:28 +0800

    fix: change go get to go install

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-//go:generate go get -u github.com/valyala/quicktemplate/qtc
+//go:generate go install github.com/valyala/quicktemplate/qtc
 //go:generate qtc -dir=templates
 func main() {
 	log.SetFormatter(&log.TextFormatter{
